f75ce5707ceca9a4292c3c3c6caca2c5291ef646
unknown <itsalexwilliams69@gmail.com>
2022-03-01 14:40:12 -0800

    network recon fixed

--- misc/netwok_scan.sh
+++ misc/netwok_scan.sh
@@ -14,5 +14,5 @@ no_ip=$(wc -l nmap_scanned_ips | awk '{print $1}')
 echo "$no_ip ip found"
 #scanning each ip
 echo "[*]Scanning each ip"
-for ip in $(cat nmap_scanned_ips); do echo "Scanning $ip" ;printf "---------------------------\n" >> ip-info.txt;nmap -sC -sV $ip >> ip-info.txt ; done
+for ip in $(cat nmap_scanned_ips); do echo "Scanning $ip" ;printf "<--------------------------->\n" >> ip-info.txt;nmap -sC -sV $ip >> ip-info.txt ; done
 printf "[*]Results stored: ip-info.txt"
